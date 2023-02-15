03f0b7d52945e34dcf10f6c3df94841afac6b83e
Vipul Patil <61792938+Vipul1705@users.noreply.github.com>
2023-02-15 16:18:08 +0530

    ENV variable added

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseApp=firebase.initializeApp({
-    apiKey: "api_key",//copy your api key here and other details
-    authDomain: "instagram-clone-101.firebaseapp.com",
-    projectId: "instagram-clone-101",
-    storageBucket: "instagram-clone-101.appspot.com",
-    messagingSenderId: "125846841100",
-    appId: "1:125846841100:web:eaa2ed3ef6a1d271b10b29",
-    measurementId: "G-VGNCZHCEPN"
+    apiKey: process.env.REACT_APP_FIREBASE_API,//copy your api key here and other details
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
 const db=firebaseApp.firestore();
